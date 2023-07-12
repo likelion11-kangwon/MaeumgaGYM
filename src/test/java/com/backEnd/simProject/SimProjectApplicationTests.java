d96ea1c7c930ca577f86d5d084642ee0e5dfe799
HanYoonSoo <hdbstn3055@gmail.com>
2023-07-12 20:01:45 +0900

    BE: Init

---
+++ src/test/java/com/backEnd/simProject/SimProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backEnd.simProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
